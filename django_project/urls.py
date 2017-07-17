774122dc77edfc8e5fd84083382a9af2d2c26eb2
Helpfulpaw <sinrayasy@gmail.com>
2017-07-18 00:39:20 +0300

    update urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from Helo import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',views.index),
 
 ]
