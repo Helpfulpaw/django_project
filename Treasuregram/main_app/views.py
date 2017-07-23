6e7b2dba55a51b12f588c2bcd9a2122357342bf0
Helpfulpaw <sinrayasy@gmail.com>
2017-07-23 11:22:13 +0300

    hello world with index

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.Http import HttpResponse
 
 def index(Request):
-    return HttpResponse('<h> Hello world !</h>')
+    return HttpResponse('<h1> Hello world !</h1>')
 
 # Create your views here.
